2c6b96fe2b7644bc728be6238f65b2af7fc0b80c
Subharthy Kuiry <subharthykuiry@gmail.com>
2025-08-10 00:09:49 +0530

    update: applied new changes

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -1,10 +1,16 @@
+import multer from 'multer';
+import path from 'path';
 
-import multer from 'multer'
+// Store files in 'uploads/' temporarily before uploading to Cloudinary
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, 'uploads/');
+  },
+  filename: function (req, file, callback) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    callback(null, uniqueSuffix + path.extname(file.originalname));
+  }
+});
 
-const storage =  multer.diskStorage({
-    filename:function(req,file,callabck){
-        callabck(null,file.originalname)
-    }
-})
-const upload = multer({storage})
-export default upload
+const upload = multer({ storage });
+export default upload;
