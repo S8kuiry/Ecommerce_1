e1961c242ddfb978e161e427446fb0701be7cd0a
Subharthy Kuiry <subharthykuiry@gmail.com>
2025-08-10 08:55:25 +0530

    update: applied new changes

--- admin/src/components/Table.jsx
+++ admin/src/components/Table.jsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
-const [loading,setLoading] = useState(false)
 
 const Table = ({ token }) => {
 
